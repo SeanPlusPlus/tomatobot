f760011c4f30ffcb7222613855fb45ecfafa3d2c
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-14 09:33:07 -0800

    main: rough initial mockup

--- components/home.js
+++ components/home.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import Image from 'next/image'
 import axios from 'axios'
 
 const Home = () => {
@@ -16,7 +17,24 @@ const Home = () => {
 
   return (
     <div className="text-center h-screen">
-      <h1 className="mt-3">Hello World</h1>
+      <div className="mx-10 mt-3">
+        <Image
+          alt="up"
+          src="/posters/up.jpg"
+          height={810}
+          width={540}
+        />
+      </div>
+      <div className="mx-10 mb-2 px-2 py-2 border rounded bg-base-100">
+        <p className="text-left text-sm">
+          An exciting, funny, and poignant adventure, Up offers an impeccably crafted story told with wit and arranged with depth, as well as yet another visual Pixar treat.
+        </p>
+      </div>
+      <div className="mx-10 px-2 py-2 border rounded bg-base-100">
+        <p className="text-left text-sm">
+          "Up" is a beautifully animated and emotionally poignant exploration of the power of love and the importance of following your dreams.
+        </p>
+      </div>
     </div>
   )
 }
